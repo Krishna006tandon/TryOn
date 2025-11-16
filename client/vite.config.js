0624aaf709fa580de12b7b1a540bcbc0c14bbd63
Rutvik Tayde <rutviktayde73@gmail.com>
2025-11-17 04:07:18 +0530

    changes in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,10 +7,10 @@ export default defineConfig({
     port: 5173,
     open: true,
   },
+  // ADD THE BUILD CONFIGURATION HERE
+  build: {
+    // Set limit to 4000 KB (4 MB) to easily clear the current warning
+    // The default is 500 KB.
+    chunkSizeWarningLimit: 4000 
+  },
 });
-
-
-
-
-
-
